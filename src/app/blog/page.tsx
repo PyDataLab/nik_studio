ee400522a1d0d46a42a26771f876186a1d941ace
akhmetshin <illife@yandex.ru>
2025-06-06 17:32:48 +0300

    06 adding about

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
